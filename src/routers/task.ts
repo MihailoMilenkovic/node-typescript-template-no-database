a11f26318775c0cd465709fd65caeaffc7fb891a
MihailoMilenkovic <mihailo.milenkovic4@gmail.com>
2021-11-20 20:25:54 +0100

    add more swagger setup

--- src/routers/task.ts
+++ src/routers/task.ts
@@ -1,11 +1,28 @@
 import express from "express";
 import axios from "axios";
 const router = new express.Router();
-
+/**
+ * @swagger
+ * /task:
+ * get:
+ *  summary: get a task
+ *  description: Use to request information about a single specific task
+ *  responses:
+ *    200:
+ *      description: Returns a single task
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ */
 router.get("/task", async (req, res) => {
   try {
-    const task = axios.get("https://jsonplaceholder.typicode.com/todos/1");
-    res.send(task);
+    const apiResponse = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+
+    console.log(apiResponse.data);
+    res.send(apiResponse.data);
   } catch (e) {
     res.status(404).send();
   }
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,51 @@
-import app from "./app";
-const port = process.env.PORT;
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
+
+import userRouter from "./routers/user";
+import taskRouter from "./routers/task";
+
+const app = express();
+
+const host = process.env.HOST || "127.0.0.1";
+const port = process.env.PORT || 3000;
+
+// Swagger config https://swagger.io/specification/#infoObject
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.1",
+    info: {
+      title: "Template API",
+      version: "1.0.0",
+      license: {
+        name: "MIT",
+      },
+      description: "Swagger documentation here",
+      contact: {
+        name: "Milenkovic Mihailo",
+        email: "mihailo.milenkovic4@gmail.com",
+      },
+      servers: [`${host}:${port}`],
+    },
+  },
+  apis: [`src/routers/*.ts`],
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+
+const swaggerUIOptions = {
+  // explorer: true,
+};
+console.log(swaggerDocs);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocs, swaggerUIOptions)
+);
+app.use(express.json());
+
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
